eedd2a2d30fb1f0a3f7263719d435df49aaeb3b3
OmBayus <omerbayramcavus@gmail.com>
2022-01-04 18:18:27 +0300

    Zreport

--- src/main/java/com/stock/app/dataAccess/ZReportDao.java
+++
@@ -1,7 +0,0 @@
-package com.stock.app.dataAccess;
-
-import com.stock.app.entities.concretes.ZReport;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ZReportDao extends JpaRepository<ZReport,Integer> {
-}
